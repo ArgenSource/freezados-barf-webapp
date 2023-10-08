46675b90bb6eeb68d76e849b9b33fa9729ebf6ff
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-10-08 16:05:06 -0300

    remove example files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { spaceRouter } from "./routers/space";
 import { createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +7,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   space: spaceRouter,
 });
 
