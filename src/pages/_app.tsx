ce506ab26877fce9f00c84fdd5ffd7e054bbd507
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-10-29 16:21:39 -0300

    refactor implicit returns and add ci

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,12 +9,10 @@ import "~/styles/globals.css";
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
-  return (
+}) => (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-};
 
 export default api.withTRPC(MyApp);
