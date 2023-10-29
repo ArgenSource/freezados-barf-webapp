2b48a2b36e1e0be6f1373b168da7c1b4fbc15312
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-10-29 12:26:42 -0300

    vitest: fix relative imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { join } from "path";
 import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     exclude: [...configDefaults.exclude, "**/e2e/**"],
     environment: "jsdom",
