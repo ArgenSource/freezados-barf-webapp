ca7a762d284186fde53e68b5a438a83ca9ab03dc
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-10-29 15:13:23 -0300

    remove debug

--- src/features/ubication/Ubication.test.tsx
+++ src/features/ubication/Ubication.test.tsx
@@ -2,7 +2,6 @@ import { test, expect } from "vitest";
 import Ubication from "./Ubication";
 import { type Ubication as TUbication } from "@prisma/client";
 import { render, screen } from "~/utils/test/test-utils";
-import { debug } from "vitest-preview";
 
 const mockUbication: TUbication = {
   id: "string",
@@ -15,5 +14,4 @@ const mockUbication: TUbication = {
 test("should render the ubication name", () => {
   render(<Ubication data={mockUbication} />);
   expect(screen.getByText("Ubication Test")).toBeDefined();
-  debug();
 });
