fdf754da56d0fc5c00d330d2399d3f07d563d33e
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-10-30 19:59:36 -0300

    Adjust required freezing days by specie

--- src/features/food/utils/isFreezingComplete.ts
+++ src/features/food/utils/isFreezingComplete.ts
@@ -12,15 +12,17 @@ export const isFreezingComplete = ({
   const difference = now - storedAtTime;
 
   const threeDaysInMilliseconds = 72 * 60 * 60 * 1000;
-  const tenDaysInMilliseconds = 240 * 60 * 60 * 1000;
+  const sevenDaysInMilliseconds = 168 * 60 * 60 * 1000;
+  const fourteenDaysInMilliseconds = 336 * 60 * 60 * 1000;
 
   switch (foodType) {
     case FoodTypes.COW:
     case FoodTypes.CHICKEN:
       return difference >= threeDaysInMilliseconds ? true : false;
     case FoodTypes.FISH:
+      return difference >= sevenDaysInMilliseconds ? true : false;
     case FoodTypes.PORK:
-      return difference >= tenDaysInMilliseconds ? true : false;
+      return difference >= fourteenDaysInMilliseconds ? true : false;
     default:
       return false;
   }
