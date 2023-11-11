b1ff2c50e8796c5c5389f3a422e5b4f3e6ccb985
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-11-10 23:26:49 -0300

    Add a link to add more ubications to a same space

--- src/features/ubication/UbicationList.tsx
+++ src/features/ubication/UbicationList.tsx
@@ -1,16 +1,24 @@
 import { type Ubication as TUbication } from "@prisma/client";
 
 import Ubication from "./Ubication";
+import Link from "next/link";
 
-export default function UbicationList({
-  ubications,
-}: {
+interface IProps {
   ubications?: TUbication[];
-}) {
+  spaceId: string;
+}
+
+export default function UbicationList({ ubications, spaceId }: IProps) {
   if (!ubications) return null;
   return (
     <>
       <h2 className="text-xl">Ubicaciones: </h2>
+      <Link
+        href={`/space/${spaceId?.toString()}/add-ubication`}
+        className="w-fit rounded-md text-gray-400 hover:text-gray-600"
+      >
+        <h3>Agregar ubicacion</h3>
+      </Link>
       {ubications.map((u) => (
         <Ubication key={u.id} data={u} />
       ))}
