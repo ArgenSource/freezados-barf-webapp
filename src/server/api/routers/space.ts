ae2de4ea5d1bc8781cc110101d233489a02cba59
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-11-14 18:20:34 -0300

    add protected to authorized procedures

--- src/server/api/routers/space.ts
+++ src/server/api/routers/space.ts
@@ -1,21 +1,31 @@
 import { createSpace, getSpace } from "~/utils/schemas/space";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 export const spaceRouter = createTRPCRouter({
-  create: publicProcedure.input(createSpace).mutation(({ input, ctx }) => ctx.db.space.create({ data: { name: input.name } })),
+  create: publicProcedure
+    .input(createSpace)
+    .mutation(({ input, ctx }) =>
+      ctx.db.space.create({ data: { name: input.name } }),
+    ),
 
-  getAll: publicProcedure.query(({ ctx }) => 
+  getAll: publicProcedure.query(({ ctx }) =>
     // TODO: Change to get only by user (when some kind of auth is implemented)
-     ctx.db.space.findMany()
+    ctx.db.space.findMany(),
   ),
 
-  getByid: publicProcedure.input(getSpace).query(({ input, ctx }) => ctx.db.space.findUnique({
+  getByid: protectedProcedure.input(getSpace).query(({ input, ctx }) =>
+    ctx.db.space.findUnique({
       where: {
         id: input.id,
       },
       include: {
         ubications: true,
       },
-    })),
+    }),
+  ),
 });
