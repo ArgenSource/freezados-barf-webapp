ea18bea572c28ad061731c2185e6a277f183392e
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-12-12 12:18:57 -0300

    adapt new schemas to zod error format

--- src/utils/schemas/space.ts
+++ src/utils/schemas/space.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
+import { SharedConfig } from "@prisma/client";
 import { notValidError, requiredError } from "./helper";
 
 const FIELD_NAMES = {
   NAME: "Nombre",
   ID: "ID",
+  SHARED_CONFIG: "Configuración de privacidad",
+  INVITATION_ID: "Código de invitación",
 };
 
 export const createSpace = z.object({
@@ -26,23 +29,50 @@ export const getSpace = z.object({
 });
 
 export const setPrivacy = z.object({
-  id: z.string().min(1),
-  config: z.enum([
-    SharedConfig.INVITATION,
-    SharedConfig.PRIVATE,
-    SharedConfig.PUBLIC_LINK,
-  ]),
+  id: z
+    .string({
+      required_error: requiredError(FIELD_NAMES.ID),
+      invalid_type_error: notValidError(FIELD_NAMES.ID),
+    })
+    .min(1, { message: requiredError(FIELD_NAMES.ID) }),
+  config: z.enum(
+    [SharedConfig.INVITATION, SharedConfig.PRIVATE, SharedConfig.PUBLIC_LINK],
+    {
+      required_error: requiredError(FIELD_NAMES.SHARED_CONFIG),
+      invalid_type_error: notValidError(FIELD_NAMES.SHARED_CONFIG),
+    },
+  ),
 });
 
 export const joinRequest = z.object({
-  spaceId: z.string().min(1),
+  spaceId: z
+    .string({
+      required_error: requiredError(FIELD_NAMES.ID),
+      invalid_type_error: notValidError(FIELD_NAMES.ID),
+    })
+    .min(1, { message: requiredError(FIELD_NAMES.ID) }),
 });
 
 export const joinWithInvitationRequest = z.object({
-  spaceId: z.string().min(1),
-  invitationId: z.string().min(1),
+  spaceId: z
+    .string({
+      required_error: requiredError(FIELD_NAMES.ID),
+      invalid_type_error: notValidError(FIELD_NAMES.ID),
+    })
+    .min(1, { message: requiredError(FIELD_NAMES.ID) }),
+  invitationId: z
+    .string({
+      required_error: requiredError(FIELD_NAMES.INVITATION_ID),
+      invalid_type_error: notValidError(FIELD_NAMES.INVITATION_ID),
+    })
+    .min(1, { message: requiredError(FIELD_NAMES.INVITATION_ID) }),
 });
 
 export const invitation = z.object({
-  spaceId: z.string().min(1),
+  spaceId: z
+    .string({
+      required_error: requiredError(FIELD_NAMES.ID),
+      invalid_type_error: notValidError(FIELD_NAMES.ID),
+    })
+    .min(1, { message: requiredError(FIELD_NAMES.ID) }),
 });
