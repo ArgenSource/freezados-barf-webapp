2433bc29477a20c3d2cc03c15275ac1430868ad6
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-12-21 15:16:05 +0100

    Add classNames prop to PageWrapper

--- src/features/common/components/PageWrapper.tsx
+++ src/features/common/components/PageWrapper.tsx
@@ -1,5 +1,14 @@
 import React, { type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-export const PageWrapper = ({ children }: PropsWithChildren) => (
-  <main className="min-h-screen bg-zinc-900 text-violet-300">{children}</main>
-);
+export const PageWrapper = ({
+  children,
+  classNames,
+}: PropsWithChildren<{ classNames?: string }>) => {
+  const styles = twMerge(
+    "min-h-screen bg-zinc-900 text-violet-300",
+    classNames,
+  );
+
+  return <main className={styles}>{children}</main>;
+};
