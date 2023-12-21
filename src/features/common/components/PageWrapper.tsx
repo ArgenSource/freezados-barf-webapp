f006b875ae930084c638d73c1a98560dc0bd4b61
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-12-21 15:14:26 +0100

    Adjust page wrapper h

--- src/features/common/components/PageWrapper.tsx
+++ src/features/common/components/PageWrapper.tsx
@@ -1,5 +1,5 @@
 import React, { type PropsWithChildren } from "react";
 
 export const PageWrapper = ({ children }: PropsWithChildren) => (
-  <main className="h-screen bg-zinc-900 text-violet-300">{children}</main>
+  <main className="min-h-screen bg-zinc-900 text-violet-300">{children}</main>
 );
