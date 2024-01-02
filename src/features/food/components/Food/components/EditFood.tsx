6d8f39d3380c2182b291ca0d3646b9add0d44bdf
Santiago Javier Rubio <s.rubio91@gmail.com>
2024-01-02 10:07:25 -0300

    add resetErrors func and implement in usecases

--- src/features/food/components/Food/components/EditFood.tsx
+++ src/features/food/components/Food/components/EditFood.tsx
@@ -22,11 +22,15 @@ export const EditFood: React.FC<ActionProps> = ({
   active,
   setSelect,
 }) => {
+  const { errors, parseErrors, resetErrors } = useFormErrors(editFood);
+
   const openModal = () => setSelect(ACTIONS.EDIT);
-  const closeModal = () => setSelect(ACTIONS.NONE);
+  const closeModal = () => {
+    resetErrors();
+    setSelect(ACTIONS.NONE);
+  };
 
   const edit = api.food.editFoodData.useMutation();
-  const { errors, parseErrors } = useFormErrors(editFood);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
