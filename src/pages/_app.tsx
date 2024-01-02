4e2630ecb46ba0bfb6eda0308e2cb4bd9f8816e4
Rodrigo <rodrigo1cabrera2@gmail.com>
2024-01-02 20:05:32 +0100

    Install Sonner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Toaster } from "sonner";
 
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
@@ -10,6 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => (
   <SessionProvider session={session}>
+    <Toaster />
     <Component {...pageProps} />
   </SessionProvider>
 );
