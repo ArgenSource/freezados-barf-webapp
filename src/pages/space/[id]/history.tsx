abe90ee48a862b979cd9414ce5980bcb32b580bc
Rodrigo <rodrigo1cabrera2@gmail.com>
2024-01-02 19:57:04 +0100

    get space id in history page

--- src/pages/space/[id]/history.tsx
+++ src/pages/space/[id]/history.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useRouter } from "next/router";
 
 import { PageLayout } from "~/features/common/components/layout";
 
 export default function SpaceHistory() {
+  const router = useRouter();
+  const { id: spaceId } = router.query;
+
+  // TODO : ADD HISTORY TABLE
   return <PageLayout headTitle="Freezados - Add new ubication">...</PageLayout>;
 }
