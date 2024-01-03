65c432397a3d00e4901fd612e8369e55756a6c2e
Santiago Javier Rubio <s.rubio91@gmail.com>
2024-01-03 12:04:28 -0300

    spell corrections

--- src/features/ubication/UbicationPage.tsx
+++ src/features/ubication/UbicationPage.tsx
@@ -13,13 +13,13 @@ import useFormErrors from "~/utils/hooks/useFormErrors";
 import { FormInput, Textarea } from "../common/components/Form";
 
 const DELETE_WARNING =
-  "¿Estás seguro que quieres eliminar esta ubicacion?\n" +
+  "¿Estás seguro que quieres eliminar esta ubicación?\n" +
   "Todos los alimentos que se encuentren en el se borrarán y no podrán ser recuperados.\n" +
   "Cámbialos de ubicación primero si deseas conservarlos";
 
 const FREEZER_RESET_WARNING =
-  "¿Estás seguro que la ubicacion no es un freezer?\n" +
-  "Si tienes alimentos en periodo de congelado su progreso se reiniciara";
+  "¿Estás seguro que la ubicación no es un freezer?\n" +
+  "Si tienes alimentos en periodo de congelado su progreso se reiniciará";
 
 export function UbicationPage({ data }: { data: TUbication }) {
   const router = useRouter();
